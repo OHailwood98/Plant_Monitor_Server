a5f879079287c6088a00e77ec51761e528b48008
OHailwood98 <41902837+OHailwood98@users.noreply.github.com>
2020-05-10 19:32:32 +0100

    make getoneweek enpoint return readings in the same format as oneday and return an error if there are no readings

--- src/routes/reading.js
+++ src/routes/reading.js
@@ -105,22 +105,26 @@ router.post("/getoneweek", (req, res) => {
       Reading.find({deviceID:id, time: { $gte: gte.toISOString(), $lte: lte.toISOString() } })
         .sort({ time: -1 })
         .then((times) => {
-          times.forEach((time) => {
-            var newTime = {
-              deviceID: time.deviceID,
-              temperature: time.temperature,
-              moisture: time.moisture,
-              light: time.light,
-              time: time.time,
-            };
-            timeArray.push(newTime);
-          });
-          for (var i = 1; i < 8; i++) {
-            var time = getWeekAverage(timeArray, i);
-            averageTimes.push(time);
-            //averageTimes = [].concat(averageTimes, time)
+          if(times.length > 0){
+            times.forEach((time) => {
+              var newTime = {
+                deviceID: time.deviceID,
+                temperature: time.temperature,
+                moisture: time.moisture,
+                light: time.light,
+                time: time.time,
+              };
+              timeArray.push(newTime);
+            });
+            for (var i = 1; i < 8; i++) {
+              var time = getWeekAverage(timeArray, i);
+              //averageTimes.push(time);
+              averageTimes = [].concat(averageTimes, time)
+            }
+            res.status(200).json({ timeList: averageTimes });
+          }else{
+            res.status(400).json({ errors: {global: "Sorry. No Readings found for this Device"} })
           }
-          res.status(200).json({ timeList: averageTimes });
         })
         .catch((err) => res.status(400).json({ errors: {global: "Server Error. Try Again"} }));
     }else{
